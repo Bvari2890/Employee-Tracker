62a41c291743dfae188d0a600b6209edfac79851
Bo Vari <bvari2890@gmail.com>
2021-10-15 10:49:11 -0400

    added prompts.js under new models folder

---
+++ models/prompts.js
@@ -0,0 +1,62 @@
+const inquirer = require('inquirer');
+
+const addDeptP = [
+    {
+        name: 'id',
+        type: 'input',
+        message: 'Enter Dept. Id:'
+    },
+{
+    name: 'name',
+    type: 'input',
+    message: 'Enter Dept. Name:'
+}
+];
+
+const addRoleP = [
+{
+    name: 'title',
+    type: 'input',
+    message: 'Enter Role Title:'
+},
+{
+    name: 'salary',
+    type: 'input',
+    message: 'Enter Role Salary:'
+},
+{
+    name: 'deptId',
+    type: 'input',
+    message: 'Roles Dept Id:',
+}
+];
+
+const addEmpP = [
+{
+    name: 'first',
+    type: 'input',
+    message: 'Enter Employee First Name:'
+},
+{
+    name: 'last',
+    type: 'input',
+    message: 'Enter Employee Last Name:'
+},
+{
+    name: 'haveManager',
+    type: 'confirm',
+    message: 'Does this employee have a manager?',
+},
+{
+    name: 'manId',
+    type: 'input',
+    message: "Enter Manager's Employee Id:"
+},
+{
+    name: 'roleId',
+    type: 'input',
+    message: 'Enter Role Id:',
+}
+];
+
+module.exports = {addRoleP, addEmpP, addDeptP}
