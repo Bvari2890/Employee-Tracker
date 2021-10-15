34e6ffc847e423f33e4ced02d98aef0d645af3c3
Bo Vari <bvari2890@gmail.com>
2021-10-15 10:48:30 -0400

    updated js code

--- index.js
+++ index.js
@@ -1,94 +1,116 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const express = require('express');
-const dotenv = require('dotenv');
-const consoleTable = require('console.table');
+require('dotenv').config();
+const cTable = require('console.table');
+const prompts = require('./models/prompts');
 
-require('console.table');
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-const promptMessages = {
-    viewAllEmployees: "View All Employees",
-    viewByDepartment: "View All Employees By Department",
-    viewByManager: "View All Employees By Manager",
-    addEmployee: "Add An Employee",
-    removeEmployee: "Remove An Employee",
-    updateRole: "Update Employee Role",
-    updateEmployeeManager: "Update Employee's Manager",
-    viewAllRoles: "View All Roles",
-    exit: "Exit"
-};
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'employees_db'
-});
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+},
+    console.log('connected to db!')
+);
 
-connection.connect(err => {
+connection.connect((err) => {
     if (err) throw err;
-    prompt();
+    console.log('WELCOME TO THE EMPLOYEE MANAGER'
+
+    )
+    startManaging();
 });
 
 
-function prompt() {
-    inquirer.prompt({
-        name: 'action',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [
-            promptMessages.viewAllEmployees,
-            promptMessages.viewByDepartment,
-            promptMessages.viewByManager,
-            promptMessages.viewAllRoles,
-            promptMessages.addEmployee,
-            promptMessages.removeEmployee,
-            promptMessages.updateRole,
-            promptMessages.exit
-        ]
+const startManaging = () => {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                //required actions
+                'Add Department',
+                'Add Role',
+                'Add Employee',
+                'View Department',
+                'View Role',
+                'View Employee',
+                'View Roster',
+                'Update Employee Info',
+                //stop program
+                'EXIT',
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'Add Department':
+                    addDept();
+                    break;
+
+                case 'Add Role':
+                    addRole();
+                    break;
+
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+
+                case 'View Department':
+                    viewDept();
+                    break;
+
+                case 'View Role':
+                    viewRole();
+                    break;
+
+                case 'View Employee':
+                    viewEmployee();
+                    break;
+
+                case 'View Roster':
+                    createConsTable();
+                    break;
+
+                case 'Update Employee Info':
+                    updateEmp();
+                    break;
+
+
+                case 'Exit':
+                    connection.end();
+                    break;
+
+                default:
+                    console.log(`Invalid action: ${answer.action}`);
+                    break;
+            }
+        });
+};
+
+let addQuery;
+
+const addDept = () => {
+    inquirer.prompt(prompts.addDeptP).then((answers) => {
+        addQuery = `INSERT INTO department (id, name) VALUES (${answers.id}, "${answers.name}")`;
+        // console.log(addQuery);
+        connection.query(addQuery, (err) => {
+            if (err) throw err;
+            startManaging();
+        });
 
     })
-    .then(answer => {
-        console.log('answer', answer);
-        switch (answer.action) {
-            case promptMessages.viewAllEmployees:
-                viewAllEmployees();
-                break;
-
-            case promptMessages.viewByDepartment:
-                viewByDepartment();
-                break;  
-                
-            case promptMessages.viewByManager:
-                viewByManager();
-                break;
-                    
-            case promptMessages.addEmployee:
-                addEmployee();
-                break;
-                        
-            case promptMessages.removeEmployee:
-                remove('delete');
-                break;
-
-            case promptMessages.updateRole:
-                remove('role');
-                break;
-                
-            case promptMessages.viewAllRoles:
-                viewAllRoles();
-                
-            case promptMessages.exit:
-                connection.end();
-                break;    
-
-
-        }
-    });
 }
 
-// adding roles, etc
+
 const addRole = () => {
     inquirer.prompt(prompts.addRoleP).then((answers) => {
         addQuery = `INSERT INTO role (title, salary, department_id) VALUES ( "${answers.title}", ${answers.salary}, "${answers.deptId}")`;
@@ -117,19 +139,7 @@ const addEmployee = () => {
     });
 }
 
-const addDept = () => {
-    inquirer.prompt(prompts.addDeptP).then((answers) => {
-        addQuery = `INSERT INTO department (id, name) VALUES (${answers.id}, "${answers.name}")`;
-        // console.log(addQuery);
-        connection.query(addQuery, (err) => {
-            if (err) throw err;
-            startManaging();
-        });
 
-    })
-}
-
-// viewing roles, etc
 const createConsTable = () => {
     connection.query("SELECT * FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.name",
     (err,res) => {
@@ -159,7 +169,7 @@ const viewRole = () => {
     startManaging();
 }
 
-// update roles, etc
+
 const updateEmp = () => {
     inquirer.prompt([{
         name: 'update',
@@ -204,7 +214,7 @@ const updateEmpMan = (empId) => {
     })
     startManaging();
  }
- 
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
