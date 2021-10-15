8df84d6f4bb731579ff1497d99536f51968878c8
Bo Vari <bvari2890@gmail.com>
2021-10-15 10:59:22 -0400

    updated employee.sql

--- db/employee.sql
+++ db/employee.sql
@@ -4,28 +4,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-   id INT NOT NULL AUTO_INCREMENT,
-   name VARCHAR (30),
-   PRIMARY KEY (id) 
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (name)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id VARCHAR(30)
+    REFERENCES department(name),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT NULL,
+    role_id INT
+    REFERENCES role(id),
+    PRIMARY KEY (id)
 );
 
